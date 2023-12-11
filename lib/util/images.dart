a8a5f8df23eb69ab62934433576dec5d139a50ed
joy25896 <joykamrujjaman59@gmail.com>
2023-12-11 19:06:55 +0600

    screenshots

--- lib/util/images.dart
+++ lib/util/images.dart
@@ -2,6 +2,8 @@ class Images {
 
   static const String placeholder = 'assets/image/placeholder.jpg';
   static const String expand = 'assets/image/expand.png';
+  static const String one = 'assets/image/1.png';
+  static const String two = 'assets/image/2.png';
 
 
 }
